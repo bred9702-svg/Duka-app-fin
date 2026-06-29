4294164160080a39b06c9a1a872a5a3c56e17494
bred9702-svg <bred9702@gmail.com>
2026-06-29 19:10:27 +0300

    Update CustomerStats.jsx

--- src/components/customer/CustomerStats.jsx
+++ src/components/customer/CustomerStats.jsx
@@ -1,7 +1,11 @@
 import Card from '../ui/Card'
 import { fmtKES } from '../../utils/formatters'
 
-export default function CustomerStats({ customer, lastPaymentLabel = 'Never' }) {
+export default function CustomerStats({
+  customer,
+  lastPaymentLabel = 'Never',
+  activeDebtCount = 0,
+}) {
   return (
     <div
       style={{
@@ -11,6 +15,7 @@ export default function CustomerStats({ customer, lastPaymentLabel = 'Never' }) 
         marginBottom: 18,
       }}
     >
+      {/* Outstanding */}
       <Card
         style={{
           padding: 14,
@@ -33,15 +38,16 @@ export default function CustomerStats({ customer, lastPaymentLabel = 'Never' }) 
             fontSize: 18,
             fontWeight: 700,
             color:
-              customer.total_owed > 0
+              (customer.total_owed || 0) > 0
                 ? '#FF6B5B'
                 : '#5FD97A',
           }}
         >
-          {fmtKES(customer.total_owed)}
+          {fmtKES(customer.total_owed || 0)}
         </p>
       </Card>
 
+      {/* Active Debts */}
       <Card
         style={{
           padding: 14,
@@ -55,7 +61,7 @@ export default function CustomerStats({ customer, lastPaymentLabel = 'Never' }) 
             marginBottom: 6,
           }}
         >
-          Payments
+          Active Debts
         </p>
 
         <p
@@ -63,13 +69,17 @@ export default function CustomerStats({ customer, lastPaymentLabel = 'Never' }) 
             fontFamily: 'var(--font-display)',
             fontSize: 20,
             fontWeight: 700,
-            color: '#5B9FF0',
+            color:
+              activeDebtCount > 0
+                ? '#F0A93D'
+                : '#5FD97A',
           }}
         >
-          {customer.payments?.length || 0}
+          {activeDebtCount}
         </p>
       </Card>
 
+      {/* Last Payment */}
       <Card
         style={{
           padding: 14,
@@ -80,10 +90,11 @@ export default function CustomerStats({ customer, lastPaymentLabel = 'Never' }) 
           style={{
             fontSize: 10,
             color: 'var(--text-low)',
-            marginBottom: 6,
+            marginBottom: 6
+            
           }}
         >
-          Last payment
+          Last Payment
         </p>
 
         <p
