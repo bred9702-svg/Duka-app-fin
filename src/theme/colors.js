c7623c0c82ea8d4a0d22ebf98eb217de074a75dd
bred9702-svg <bred9702@gmail.com>
2026-06-30 18:59:15 +0300

    Create colors.js

---
+++ src/theme/colors.js
@@ -0,0 +1,25 @@
+const colors = {
+  primary: '#F0A93D',
+
+  success: '#5FD97A',
+
+  danger: '#FF5C5C',
+
+  warning: '#FFB648',
+
+  info: '#5B9DFF',
+
+  background: '#0F1117',
+
+  surface: '#171B22',
+
+  border: 'rgba(255,255,255,.08)',
+
+  text: '#FFFFFF',
+
+  textSecondary: '#A8AFBC',
+
+  textMuted: '#70798B',
+}
+
+export default colors
